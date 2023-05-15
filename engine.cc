60ba69442921a28c35626a318db0d8bc8f6aba86
oliviaolijf <olivia.wuyts@student.uantwerpen.be>
2023-05-15 13:46:19 +0200

    3DL ding kapoet

--- engine.cc
+++ engine.cc
@@ -93,7 +93,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
 
         int iter = 0;
         std::string full_string = initiator;
-        while (iter < iterations) { //Terug veranderen naar iterations!!
+        while (iter < iterations) {
             std::string temp;
             for (auto &c: full_string) {
                 if (c != '+' && c != '-' && c != '(' && c != ')') {
@@ -197,8 +197,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 fig.color = drawingcolor;
                 applyTransformation(fig, allTrans);
                 figures.push_back(fig);
-            }
-            else if (type2 == "Cube"){
+            } else if (type2 == "Cube") {
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
                 auto rotatY = configuration[figure_name]["rotateY"].as_double_or_die();
                 auto rotatZ = configuration[figure_name]["rotateZ"].as_double_or_die();
@@ -220,8 +219,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto cube = generateCube(figureColor);
                 applyTransformation(cube, allTrans);
                 figures.push_back(cube);
-            }
-            else if (type2 == "Tetrahedron"){
+            } else if (type2 == "Tetrahedron") {
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
                 auto rotatY = configuration[figure_name]["rotateY"].as_double_or_die();
                 auto rotatZ = configuration[figure_name]["rotateZ"].as_double_or_die();
@@ -243,8 +241,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto cube = generateTetrahedron(figureColor);
                 applyTransformation(cube, allTrans);
                 figures.push_back(cube);
-            }
-            else if (type2 == "Octahedron"){
+            } else if (type2 == "Octahedron") {
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
                 auto rotatY = configuration[figure_name]["rotateY"].as_double_or_die();
                 auto rotatZ = configuration[figure_name]["rotateZ"].as_double_or_die();
@@ -266,8 +263,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto cube = generateOctahedron(figureColor);
                 applyTransformation(cube, allTrans);
                 figures.push_back(cube);
-            }
-            else if (type2 == "Icosahedron"){
+            } else if (type2 == "Icosahedron") {
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
                 auto rotatY = configuration[figure_name]["rotateY"].as_double_or_die();
                 auto rotatZ = configuration[figure_name]["rotateZ"].as_double_or_die();
@@ -289,8 +285,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto cube = generateIcosahedron(figureColor);
                 applyTransformation(cube, allTrans);
                 figures.push_back(cube);
-            }
-            else if (type2 == "Dodecahedron"){
+            } else if (type2 == "Dodecahedron") {
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
                 auto rotatY = configuration[figure_name]["rotateY"].as_double_or_die();
                 auto rotatZ = configuration[figure_name]["rotateZ"].as_double_or_die();
@@ -312,8 +307,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto cube = generateDodecahedron(figureColor);
                 applyTransformation(cube, allTrans);
                 figures.push_back(cube);
-            }
-            else if (type2 == "Sphere"){
+            } else if (type2 == "Sphere") {
                 auto iterations = configuration[figure_name]["n"].as_int_or_die();
 
                 auto rotatX = configuration[figure_name]["rotateX"].as_double_or_die();
@@ -337,8 +331,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto sphere = generateSphere(figureColor, iterations);
                 applyTransformation(sphere, allTrans);
                 figures.push_back(sphere);
-            }
-            else if (type2 == "Cone"){
+            } else if (type2 == "Cone") {
                 auto iterations = configuration[figure_name]["n"].as_int_or_die();
                 auto height = configuration[figure_name]["height"].as_double_or_die();
 
@@ -363,8 +356,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto sphere = generateCone(figureColor, iterations, height);
                 applyTransformation(sphere, allTrans);
                 figures.push_back(sphere);
-            }
-            else if (type2 == "Cylinder"){
+            } else if (type2 == "Cylinder") {
                 auto iterations = configuration[figure_name]["n"].as_int_or_die();
                 auto height = configuration[figure_name]["height"].as_double_or_die();
 
@@ -389,8 +381,7 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto sphere = generateCylinder(figureColor, iterations, height);
                 applyTransformation(sphere, allTrans);
                 figures.push_back(sphere);
-            }
-            else if (type2 == "Torus"){
+            } else if (type2 == "Torus") {
                 auto r = configuration[figure_name]["r"].as_double_or_die();
                 auto R = configuration[figure_name]["R"].as_double_or_die();
                 auto n = configuration[figure_name]["n"].as_double_or_die();
@@ -414,11 +405,10 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto figure_color = configuration[figure_name]["color"].as_double_tuple_or_die();
                 Color figureColor = Color(figure_color[0], figure_color[1], figure_color[2]);
 
-                auto sphere = generateTorus(figureColor, r, R, n ,m);
+                auto sphere = generateTorus(figureColor, r, R, n, m);
                 applyTransformation(sphere, allTrans);
                 figures.push_back(sphere);
-            }
-            else if (type2 == "3DLSystem"){
+            } else if (type2 == "3DLSystem") {
                 Figure fig;
                 auto inputfile = configuration[figure_name]["inputfile"].as_string_or_die();
                 std::ifstream input(inputfile);
@@ -448,33 +438,109 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
                 auto angle = lsystem.get_angle();
                 auto iterations = lsystem.get_nr_iterations();
 
-                angle = angle*M_PI/180;
+                angle = angle * M_PI / 180;
 
-                Vector3D curp = Vector3D::point(0,0,0);
-                Vector3D H = Vector3D::point(1,0,0);
-                Vector3D L = Vector3D::point(0,1,0);
-                Vector3D U = Vector3D::point(0,0,1);
+                int pointcounter = 0;
 
-                int counter = 0;
-                std::vector<Vector3D> pointstack;
-                std::vector<Vector3D> Hstack;
-                std::vector<Vector3D> Lstack;
-                std::vector<Vector3D> Ustack;
+                Vector3D curp = Vector3D::point(0, 0, 0);
+                Vector3D H = Vector3D::point(1, 0, 0);
+                Vector3D L = Vector3D::point(0, 1, 0);
+                Vector3D U = Vector3D::point(0, 0, 1);
 
                 fig.points.push_back(curp);
 
-                for (int i = 0; i < iterations; i++){
+                int counter = 0;
+                std::vector<Vector3D> pointstack;
+                std::vector<Vector3D> Hstack; // x-as
+                std::vector<Vector3D> Lstack; // y-as
+                std::vector<Vector3D> Ustack; // z-as
+
+                for (int i = 0; i < iterations; i++) {
                     std::string temp;
-                    for (auto s: initiator){
-                        if (s == '+') temp+="+";
-                        else if (s == '-') temp+="-";
-                        else if (s == '^') temp +="^";
-                        else if (s == '&') temp+="&";
-                        else if (s == '\\') temp +="\\"
+                    for (auto &s: initiator) {
+                        if (s == '+') temp += "+";
+                        else if (s == '-') temp += "-";
+                        else if (s == '^') temp += "^";
+                        else if (s == '&') temp += "&";
+                        else if (s == '\\') temp += "\\";
+                        else if (s == '/') temp += "/";
+                        else if (s == '|') temp += "|";
+                        else if (alfabet.count(s)) {
+                            auto m = lsystem.get_replacement(s);
+                            temp += m;
+                        }
+                    }
+                    initiator = temp;
+                }
+                auto test = initiator.length();
+                for (auto &c: initiator) {
+                    if (alfabet.count(c)) {
+                        if (lsystem.draw(c)) {
+                            curp += H;
+                            pointcounter++;
+                            Face face({pointcounter, pointcounter-1});
+                            fig.faces.push_back(face);
+                            fig.points.push_back(curp);
+                        } else {
+                            curp += H;
+                        }
+                    } if (c == '+') {
+                        auto temp = H;
+                        H = (H*cos(angle)) + (L* sin(angle));
+                        L = -(temp*sin(angle)) + (L*cos(angle));
+                    }
+                    if (c == '-') {
+                        auto temp = H;
+                        H = (H*cos(-angle)) + (L* sin(-angle));
+                        L = -(temp*sin(-angle)) + (L*cos(-angle));
+                    }
+                    if (c == '^'){
+                        auto temp = H;
+                        H = (H*cos(angle)) + (U* sin(angle));
+                        U = (temp*sin(angle)) + (U*cos(angle));
+                    }
+                    if (c == '&') {
+                        auto temp = H;
+                        H = (H* cos(-angle)) + (U* sin(-angle));
+                        U = (temp* sin(-angle)) + (U*cos(-angle));
+                    }
+                    if (c == '\\'){
+                        auto temp = L;
+                        L = (L* cos(angle)) - (U*sin(angle));
+                        U = (temp*sin(angle)) + (U* cos(angle));
+                    }
+                    if (c == '/'){
+                        auto temp = L;
+                        L = (L*cos(-angle)) - (U* sin(-angle));
+                        U = (temp*sin(-angle)) + (U*cos(-angle));
+                    }
+                    if (c == '|'){
+                        H = -H;
+                        L = -L;
+                    }
+                    if (c == '('){
+                        pointstack.push_back(curp);
+                        Hstack.push_back(H);
+                        Lstack.push_back(L);
+                        Ustack.push_back(U);
+                    }
+                    if (c == ')'){
+                        curp = pointstack[pointstack.size()-1];
+                        pointstack.erase(pointstack.end());
+
+                        H =  Hstack[Hstack.size()-1];
+                        Hstack.erase(Hstack.end());
+
+                        L = Lstack[Lstack.size()-1];
+                        Lstack.erase(Lstack.end());
+
+                        U = Ustack[Ustack.size()-1];
+                        Ustack.erase(Ustack.end());
                     }
                 }
+                fig.color = figureColor;
+                figures.push_back(fig);
             }
-
         }
         auto lijntjes = doProjection(figures);
         auto img = Draw2DLines(lijntjes, size, bgcolor);
@@ -485,78 +551,14 @@ img::EasyImage generate_image(const ini::Configuration &configuration) {
 
 
 int main(int argc, char const *argv[]) {
-    int retVal = 0;
-    try
-    {
-        std::vector<std::string> args = std::vector<std::string>(argv+1, argv+argc);
-        if (args.empty()) {
-            std::ifstream fileIn("filelist");
-            std::string filelistName;
-            while (std::getline(fileIn, filelistName)) {
-                args.push_back(filelistName);
-            }
-        }
-        for(std::string fileName : args)
-        {
-            ini::Configuration conf;
-            try
-            {
-                std::ifstream fin(fileName);
-                if (fin.peek() == std::istream::traits_type::eof()) {
-                    std::cout << "Ini file appears empty. Does '" <<
-                              fileName << "' exist?" << std::endl;
-                    continue;
-                }
-                fin >> conf;
-                fin.close();
-            }
-            catch(ini::ParseException& ex)
-            {
-                std::cerr << "Error parsing file: " << fileName << ": " << ex.what() << std::endl;
-                retVal = 1;
-                continue;
-            }
+    std::ifstream input;
+    input.open("wireframes011.ini");
+    ini::Configuration conf;
+    input>>conf;
 
-            img::EasyImage image = generate_image(conf);
-            if(image.get_height() > 0 && image.get_width() > 0)
-            {
-                std::string::size_type pos = fileName.rfind('.');
-                if(pos == std::string::npos)
-                {
-                    //filename does not contain a '.' --> append a '.bmp' suffix
-                    fileName += ".bmp";
-                }
-                else
-                {
-                    fileName = fileName.substr(0,pos) + ".bmp";
-                }
-                try
-                {
-                    std::ofstream f_out(fileName.c_str(),std::ios::trunc | std::ios::out | std::ios::binary);
-                    f_out << image;
-
-                }
-                catch(std::exception& ex)
-                {
-                    std::cerr << "Failed to write image to file: " << ex.what() << std::endl;
-                    retVal = 1;
-                }
-            }
-            else
-            {
-                std::cout << "Could not generate image for " << fileName << std::endl;
-            }
-        }
-    }
-    catch(const std::bad_alloc &exception)
-    {
-        //When you run out of memory this exception is thrown. When this happens the return value of the program MUST be '100'.
-        //Basically this return value tells our automated test scripts to run your engine on a pc with more memory.
-        //(Unless of course you are already consuming the maximum allowed amount of memory)
-        //If your engine does NOT adhere to this requirement you risk losing points because then our scripts will
-        //mark the test as failed while in reality it just needed a bit more memory
-        std::cerr << "Error: insufficient memory" << std::endl;
-        retVal = 100;
-    }
-    return retVal;
+    std::ofstream out;
+    out.open("test1.bmp");
+    auto img = generate_image(conf);
+    out << img;
+    return 0;
 }
